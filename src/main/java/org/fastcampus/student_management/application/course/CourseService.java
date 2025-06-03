8f65846e6ac6fe55a1f3b9ce1f240029129cc707
한상희 <tkdgml822@gmail.com>
2025-06-03 15:32:35 +0900

    feat: CourseService 가격 변경, 요일 조회 구현

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -1,5 +1,6 @@
 package org.fastcampus.student_management.application.course;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
@@ -26,10 +27,24 @@ public class CourseService {
 
   public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
     // TODO: 과제 구현 부분
-    return new ArrayList<>();
+    List<Course> courseDayOfWeek = courseRepository.getCourseDayOfWeek(dayOfWeek);
+    List<CourseInfoDto> results = new ArrayList<>();
+
+    for (Course course : courseDayOfWeek) {
+      results.add(new CourseInfoDto(course));
+    }
+
+    return new ArrayList<>(results);
   }
 
+  // 학생, 가격
   public void changeFee(String studentName, int fee) {
     // TODO: 과제 구현 부분
+    List<Course> courseListByStudent = courseRepository.getCourseListByStudent(studentName);
+
+    for (Course course : courseListByStudent) {
+      Student student = studentService.getStudent(course.getStudentName());
+      courseRepository.save(new Course(student, course.getCourseName(), fee, course.getDayOfWeek(), course.getCourseTime()));
+    }
   }
 }
